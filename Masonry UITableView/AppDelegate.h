5f557f5469134572e784a293f3565c0c3b00488f
Shane Mileham <shane@bond.sh>
2016-05-17 12:47:27 -0700

    Initial Commit

---
+++ Masonry UITableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry UITableView
+//
+//  Created by Bond on 5/17/16.
+//  Copyright © 2016 Bond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
